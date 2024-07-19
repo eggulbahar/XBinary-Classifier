99b3fd237bbff65ac795f79ac0e2e0e842e72819
Hemanth <107019973+Hemanthb1@users.noreply.github.com>
2024-07-18 22:53:06 -0400

    Update classifier.py

--- classify/classifier.py
+++ classify/classifier.py
@@ -3,27 +3,33 @@ from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import confusion_matrix
 import numpy as np
-class binaryclassify:
+class BinaryClassifier:
     
-    def __init__(self,X,y,n_neighbors=4):
-        self.X = X
-        self.y = y
+    def __init__(self,X_train,y_train,n_neighbors=4):
+        self.X_train = X_train
+        self.y_train= y_train
         self.n_neighbors = n_neighbors
         self.knn = KNeighborsClassifier(n_neighbors=self.n_neighbors)
         
-    def predict(self):
-        X_train, X_test, y_train, y_test = train_test_split(self.X, self.y, random_state=0)
+    def train(self):
+        self.knn.fit(self.X_train, self.y_train)
         
-        
-        KNN_fit=self.knn.fit(X_train, y_train)
+    def predict(self, X_test):
+        preds = self.knn.predict(X_test)
+        return preds
+    
+    def evaluate(self, X_test, y_test):
         accuracy = self.knn.score(X_test, y_test)
-        
         print(f'KNN score: {accuracy}\n')
         
-        cv_scores = cross_val_score(self.knn, self.X, self.y, cv=3)
-        print('Cross-validation scores (3-fold):', cv_scores)
-        print('Mean cross-validation score (3-fold): {:.3f}'.format(np.mean(cv_scores)))
-        preds = KNN_fit.predict(X_test)
-        cm = confusion_matrix(y_test, preds)
-        sns.heatmap(cm, annot=True, cmap='viridis')
-        return preds
+        cm = confusion_matrix(y_test, self.predict(X_test))
+        
+        # Plot confusion matrix
+        plt.figure(figsize=(8, 6))
+        sns.heatmap(cm, annot=True, cmap='viridis', fmt='d')
+        plt.xlabel('Predicted')
+        plt.ylabel('Actual')
+        plt.title('Confusion Matrix')
+        plt.show()
+        
+        return cm, accuracy
